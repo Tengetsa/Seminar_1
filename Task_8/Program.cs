e4b858e474b63e7d5c28f73e74dde336d9f30121
Roman <i@zangetsu.ru>
2022-08-21 21:10:43 +0500

    Task4Up

--- Task_8/Program.cs
+++ Task_8/Program.cs
@@ -7,6 +7,6 @@ int Numbers = 1;
 
 while (Numbers <= N)
 {
-     if (Numbers%2 == 0) Console.WriteLine($"Число {Numbers} четное ");
+     if (Numbers%2 == 0) Console.Write($"{Numbers} ");
     Numbers++;
 }
